1c77094b70a283cc1e157e8b8070497b067be856
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-03-08 21:36:33 -0800

    Done, with difficulty, a lot of going back to previous lesson to follow construct, need to be better

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+/* Deliverables 
+   when button element clicked, the item should be added to the virtual
+   cart
+   
+   if item not in cart, the <button> text should read "Add to Cart"
+
+   if item in cart, <button> text should read "Remove From Cart"
+   */
+  const [inCart, setInCart] = useState(false)
+  const cartState = inCart ? "in-cart" : ""
+  const addOrRemove = inCart ? "remove" : "add"
+
+  function itemHandling() {
+    setInCart((inCart) => !inCart)
+  }
   return (
-    <li className="">
+    <li className={cartState}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addOrRemove} onClick={itemHandling}>{inCart ? "Remove From" : "Add To"} Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import items from "../data/items";
 import Item from "./Item";
 
+/* Deliverables:
+   the <select> element will allow us to filter the list of items by
+   category
+
+   use the useState hook to create variable named "selectedCategory"
+   for keeping track of the selected value
+
+   when the value of the <select> element is changed, update state
+
+   use selectedCategory variable to determine which items to display
+   in the shopping list
+
+   filter the array of items based on the selectedCategory
+*/
+
+
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectedCategory
+    }
+  })
+  function handleFilter(e) {
+    setSelectedCategory(e.target.value)
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +44,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
